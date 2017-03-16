46286ae4b49612f53a0924674f95f8dff5766082
Brandon Kroes <b.kroes@protonmail.com>
2017-03-16 11:52:30 +0100

    new fixes codeclimate

--- wp-flash-messages.php
+++ wp-flash-messages.php
@@ -15,9 +15,8 @@ class WPFlashMessages {
 	 /**
 		* WPFlashMessages constructor.
 		*/
-	 public function __construct()
-	 {
-			add_action('admin_notices', array(&$this, 'show_flash_messages'));
+	 public function __construct() {
+			add_action('admin_notices', array ( &$this, 'show_flash_messages' ) );
 	 }
 
 	 /**
@@ -27,16 +26,15 @@ class WPFlashMessages {
 		* @return bool | false if queuing failed | true if queuing succeeded
 		* @throws Exception
 		*/
-	 public static function queue_flash_message($message, $class = '')
-	 {
+	 public static function queue_flash_message($message, $class = '' ) {
 			if (empty($message)) {
-				throw new \Exception("message is empty");
+				throw new \Exception('message is empty');
 			}
 
-			$message = sanitize_text_field($message);
+			$message = sanitize_text_field( $message );
 
 			if (isset($class)) {
-				$class = sanitize_text_field($class);
+				$class = sanitize_text_field( $class );
 			}
 
 			$default_allowed_classes = array('error', 'updated', 'alert-danger');
@@ -63,18 +61,17 @@ class WPFlashMessages {
 		* @return bool|int
 		* @throws Exception
 		*/
-	 public static function find_flash_message_from_queue($message, $class = '')
-	 {
+	 public static function find_flash_message_from_queue( $message, $class = '' ) {
 
 			if (empty($message)) {
-				throw new \Exception("message is empty");
+				throw new \Exception('message is empty');
 			}
 			if (empty($class)) {
 				throw new \Exception("class is empty");
 			}
 
-			$message = sanitize_text_field($message);
-			$class = sanitize_text_field($class);
+			$message = sanitize_text_field( $message );
+			$class = sanitize_text_field( $class );
 
 			$record = false;
 			for ($i = 0; $i < count($_SESSION['wp_flash_messages'][$class]); $i++) {
@@ -99,20 +96,19 @@ class WPFlashMessages {
 		* @return bool
 		* @throws Exception
 		*/
-	 public static function delete_flash_message_from_queue($message, $class = '')
-	 {
+	 public static function delete_flash_message_from_queue( $message, $class = '' ) {
 
 			if (empty($message)) {
-				throw new \Exception("message is empty");
+				throw new \Exception('message is empty');
 			}
 			if (empty($class)) {
 				throw new \Exception("class is empty");
 			}
 
-			$message = sanitize_text_field($message);
-			$class = sanitize_text_field($class);
+			$message = sanitize_text_field( $message );
+			$class = sanitize_text_field( $class );
 
-			$found = self::find_flash_message_from_queue($message, $class);
+			$found = self::find_flash_message_from_queue( $message, $class);
 
 			//$found = (int)$found;
 
@@ -121,7 +117,7 @@ class WPFlashMessages {
 				/*
 				* Due to the nature of unset, there won't be a return if it succeeded or not hence the double find
 				 */
-				if (is_bool(self::find_flash_message_from_queue($message, $class))) {
+				if (is_bool(self::find_flash_message_from_queue( $message, $class))) {
 					 return false;
 				}
 				return true;
@@ -139,14 +135,13 @@ class WPFlashMessages {
 		* @return bool | false if flash message doesn't exist | true if message exists
 		* @throws Exception
 		*/
-	 public static function check_flash_message_exists($message, $class = '')
-	 {
+	 public static function check_flash_message_exists($message, $class = '' ) {
 			//Message must be filled, class can be empty
 			if (empty($message)) {
-				throw new \Exception("message is empty");
+				throw new \Exception('message is empty');
 			}
 
-			$message = sanitize_text_field($message);
+			$message = sanitize_text_field( $message );
 
 
 			if (isset($_SESSION['wp_flash_messages'])) {
@@ -165,7 +160,7 @@ class WPFlashMessages {
 				}
 
 				//Search through a specific class, class can be defined here due to the nature of the search
-				$class = sanitize_text_field($class);
+				$class = sanitize_text_field( $class );
 				foreach ($array[$class] as $row => $row_message) {
 					 if ($row_message == $message) {
 							return true;
@@ -183,8 +178,7 @@ class WPFlashMessages {
 		* If it doesn't contain the rest of the function serves no purpose
 		* This function needs to be called on every page you want the notifications to be displayed.
 		*/
-	 public static function show_flash_messages()
-	 {
+	 public static function show_flash_messages() {
 			if (isset($_SESSION['wp_flash_messages'])) {
 				$flash_messages = maybe_unserialize($_SESSION['wp_flash_messages']);
 
@@ -207,8 +201,7 @@ class WPFlashMessages {
 		* Checks if the array is the array contains any classes, a class must be connected to a message
 		* @return bool | if the array is empty : true | if the array is filled : false
 		*/
-	 public static function check_if_empty()
-	 {
+	 public static function check_if_empty() {
 			//If the value isn't set the array is undefined therefore empty
 			if (isset($_SESSION['wp_flash_messages'])) {
 
